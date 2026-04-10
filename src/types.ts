0912dadf804ace3ffc662fb86440f2623b1f8f21
merveille <gracehouedanou63@gmail.com>
2026-04-10 11:06:38 -0400

    modifications

--- src/types.ts
+++ src/types.ts
@@ -1,23 +1,21 @@
 export type Statut = "A faire" | "En cours" | "Terminé";
 
-export type Tache = { /*Les noms des champs ne sont pas très parlant. Par exemple text et bought n'ont pas vraiment de sens.*/
+export type Tache = {
   id: number;
   input: string;
   statut: Statut;
   description: string;
   task: boolean;
 };
-export type Filtre = ["Tous", "A faire", "En cours", "Terminé"];
+
+export type Filtre = "Tous" | "A faire" | "En cours" | "Terminé";
 
 export type Action =
   | {
       type: "AJOUTER";
       payload: {
-<<<<<<< HEAD
         input: string;
-=======
-        text: string; /*Champ mal nommé*/
->>>>>>> 7f5872f1270389ab47472e1af38e42aeacd0e6cb
+        text: string;
         description: string;
         statut: Statut;
       };
@@ -27,11 +25,11 @@ export type Action =
       payload: { id: number };
     }
   | {
-<<<<<<< HEAD
       type: "TOGGLE";
-=======
-      type: "TOGGLE_BOUGHT"; /*Pourquoi la nomination bought*/
->>>>>>> 7f5872f1270389ab47472e1af38e42aeacd0e6cb
+      payload: { id: number };
+    }
+  | {
+      type: "TOGGLE_";
       payload: { id: number };
     }
   | {
